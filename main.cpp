7053a4a2a4f06cafddd526a4d3e58aaa62014702
firmwarezhu <firmwarezhu@126.com>
2024-10-19 11:36:59 +0800

    rename the project to BitTabs

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.setWindowTitle("Bit Master");
+    w.setWindowTitle("BitTabs");
     w.show();
     return a.exec();
 }
